2a79a91ee3d8dc8eb6e252dfea2536ef3db4c211
nijz <nijenbhut@gmail.com>
2025-07-11 13:04:21 +0530

    middlewear enhanced

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -7,7 +7,6 @@ export const auth = (req, res, next) => {
     try {
 
         const token = req.cookies.token || req.header("Authorization")?.replace("Bearer ", "");
-        console.log(token);
 
         if (!token) {
             return res.status(401).json({
